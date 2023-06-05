ddc0c4010e4427a2992cf4e7b383b6d6f8cf0799
henriquepbalsimelli <henriquepbalsimellil@outlook.com>
2023-06-05 08:06:34 -0300

    Start

---
+++ src/InsertionSort.java
@@ -0,0 +1,21 @@
+
+public class InsertionSort {
+	public static void main(String[] args) {
+		Produto produtos[] = new Produto[5];
+		produtos[0] = new Produto ("Lamborghini", 1000000);
+		produtos[1] = new Produto("Jipe", 46000);
+	    produtos[2]= new Produto("Brasília", 16000);
+	    produtos[3] = new Produto("Smart", 46000);
+	    produtos[4] = new Produto("Fusca", 17000);
+	    
+	    int maisBarato = 0;
+	    for(int atual = 0; atual <= 4; atual++){
+	        if(produtos[atual].getPreco() < produtos[maisBarato].getPreco()) {
+	            maisBarato = atual;
+	        }
+	    }
+	    System.out.println(produtos[maisBarato].getName());
+	    System.out.println("O carro mais barato custa "
+	                                 + produtos[maisBarato].getPreco());
+	}
+}
---
+++ src/Produto.java
@@ -0,0 +1,20 @@
+
+public class Produto {
+
+	String nome;
+	double preco;
+	
+	public Produto(String nome, double preco) {
+		this.nome = nome;
+		this.preco = preco;
+	}
+	
+	public String getName() {
+		return nome;
+	}
+	
+	public double getPreco() {
+		return preco;
+	}
+
+}
